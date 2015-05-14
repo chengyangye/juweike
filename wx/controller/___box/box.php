483541a302cec70abb9f088b9af2586c44e511aa
chengyangye <chengyang.ye@gmail.com>
2015-05-14 16:08:38 +0800

    del

--- wx/controller/___box/box.php
+++
@@ -1,5 +0,0 @@
-<?php
-$wid = $_GET['wid'];//Session::get('wid');
-$list = new Model('micro_music_list');
-$list1 = $list->where(array('wid'=>$wid))->order('id','name','ms','pic')->list_all();//map_array('name','ms');
-
--- yyuc/clear_logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f ./wx/log/error/*
-rm -f ./wx/public_html/admin2/data/runtime/Log/*
-rm -f ./wx/public_html/data/runtime/Log/*
